c635c3f0e7d7c5fac8f5f98c7162c069d6c85261
Lakshmiredddy <awsd851@gmail.com>
2025-07-10 00:11:26 +0530

    shell

--- 06-array.sh
+++ 06-array.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+NAME=("Ram" "sita" "Lakshman")
 
-MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
-
-echo "First Movie: ${MOVIES[0]}"
-echo "First Movie: ${MOVIES[3]}"
-echo "First Movie: ${MOVIES[4]}"
-
-echo "All movies: ${MOVIES[@]}"
+echo "${NAME[0]}"
+echo "${NAME[1]}"
+echo "${NAME[2]}"
