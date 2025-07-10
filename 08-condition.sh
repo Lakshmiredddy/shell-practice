5820ed31ed736cb7aaeef0096e72360b6cedac20
Lakshmiredddy <awsd851@gmail.com>
2025-07-11 01:08:38 +0530

    shell

--- 08-condition.sh
+++ 08-condition.sh
@@ -2,13 +2,13 @@
 
 NUM=60
 
-if [ $NUM > 50 ]
+if [ $NUM -gt 50 ]
 then
 echo " number is greather than $NUM "
 fi
 
 
-if [ $NUM < 50 ]
+if [ $NUM -lt 50 ]
 then
 echo " number is less than $NUM "
 fi
